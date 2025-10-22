81ae4f80ab2ceca7b974498dde0b2b1987c08cb5
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-10-21 22:20:13 -0300

    feat(singleton): :sparkles: add singleton pattern

---
+++ singleton/Logger.js
@@ -0,0 +1,20 @@
+class SingletonLogger {
+  constructor() {
+    if (SingletonLogger.instance) {
+      return SingletonLogger.instance;
+    }
+    this.logs = [];
+    SingletonLogger.instance = this;
+  }
+
+  log(message) {
+    this.logs.push(message);
+    console.log(`Mensagem: ${message}`);
+  }
+
+  print() {
+    console.log(this.logs.length);
+  }
+}
+
+module.exports = SingletonLogger;
