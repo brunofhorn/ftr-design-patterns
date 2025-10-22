417c41054c10f2acdcf7b962d14489712321496b
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-10-21 22:51:31 -0300

    feat(observer): :sparkles: add a observer pattern

---
+++ observer/testObserver.js
@@ -0,0 +1,15 @@
+const Cliente = require("./models/ConcreteObjserver");
+const Loja = require("./models/ConcreteSubject");
+
+const loja = new Loja();
+const clienteA = new Cliente("Carlos");
+const clienteB = new Cliente("Ana");
+const clienteC = new Cliente("Beto");
+
+loja.adicionarObservers(clienteA);
+loja.adicionarObservers(clienteB);
+loja.adicionarObservers(clienteC);
+
+loja.removerObservers(clienteA);
+
+loja.receberNovoProduto();
