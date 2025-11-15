f889dab498f473ed10728d402a84235e9b87e776
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-11-15 00:41:40 -0300

    feat(add initial front): :sparkles: add input and button form to generate caption

--- hospedagem-ia/legendas-para-imagens/front/src/App.jsx
+++ hospedagem-ia/legendas-para-imagens/front/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imgSrc, setImgSrc] = useState(null);
+  const [caption, setCaption] = useState("<Caption>");
+
+  function generateCaption() {
+    setCaption("Novo Caption");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Caption Generator</h1>
+      <div className="url-form">
+        <input onChange={(e) => setImgSrc(e.target.value)} />
+        <button onClick={generateCaption}>Generate</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="captioned-image">
+        <img src={imgSrc} height={200} style={{ marginBottom: "10px" }} />
+        <span>{caption}</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
